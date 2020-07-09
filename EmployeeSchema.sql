9ad3169d681a2a463ca5ca2e7b1d357f1438c419
Crystal <crystalwatkins428@gmail.com>
2020-07-09 16:47:34 -0400

    updated sql routes and tables

--- EmployeeSchema.sql
+++ EmployeeSchema.sql
@@ -27,41 +27,38 @@ CREATE TABLE employee (
 );
 
 
-INSERT into department (department_name) VALUES ("Sales");
-INSERT into department (department_name) VALUES ("Engineer");
-INSERT into department (department_name) VALUES ("Finance");
-INSERT into department (department_name) VALUES ("Legal");
-
-INSERT into role (title, salary, department_id) VALUES ("Sales Manager", 100000, 1);
-INSERT into role (title, salary, department_id) VALUES ("Sales person", 50000, 1);
-INSERT into role (title, salary, department_id) VALUES ("Lead Enginneer", 100000, 2);
-INSERT into role (title, salary, department_id) VALUES ("Junior Engineer", 900000, 2);
-INSERT into role (title, salary, department_id) VALUES ("Accountant", 100000, 3);
-INSERT into role (title, salary, department_id) VALUES ("Legal Team Employee", 30000, 4);
-INSERT into role (title, salary, department_id) VALUES ("Lawyer", 30000, 4);
-
-
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Doe", 1, 2);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Mary Kate", "Olsen", 1, 3);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Ashley", "Olsen", 2, 4);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Michael", "Scott", 1, null);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Jim", "Halpert", 3, 1);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Ron", "Swanson", 2, null);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Leslie", "Knope", 4, 2);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Phoebe", "Buffet", 3, null);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Rachel", "Green", 1, 3);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Monica", "Geller", 2, 4);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Ross", "Geller", 3, 1);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Chandler", "Bing", 4, 2);
-INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("Andy", "Dwyer", 4, null);
+INSERT into department (department_name) VALUES ("Sales"), ("Engineer"), ("Finance"), ("Legal");
+INSERT into role (title, salary, department_id) VALUES ("Sales Manager", 100000, 1), ("Sales person", 50000, 1),
+("Lead Enginneer", 100000, 2),("Junior Engineer", 900000, 2), ("Accountant", 100000, 3),
+("Legal Team Employee", 30000, 4), (title, salary, department_id) VALUES ("Lawyer", 30000, 4);
+INSERT into employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Doe", 1, 2),
+("Mary Kate", "Olsen", 1, 3),("Ashley", "Olsen", 2, 4),("Michael", "Scott", 1, null), ("Jim", "Halpert", 3, 1),
+("Ron", "Swanson", 2, null), ("Leslie", "Knope", 4, 2), ("Phoebe", "Buffet", 3, null),("Rachel", "Green", 1, 3),
+("Monica", "Geller", 2, 4),("Ross", "Geller", 3, 1),("Chandler", "Bing", 4, 2), ("Andy", "Dwyer", 4, null);
 
 SELECT * FROM department;
 SELECT * FROM role;
 SELECT * FROM employee;
 
--- SELECT department.id, department.department_name, role.id, role.title, role.salary, role.department_id, employee.first_name, employee.last_name, employee.role_id, employee_manager.id
--- FROM department
--- RIGHT JOIN department ON (department.department_name = role.id AND on role.department_id = employee.first_name
--- ORDER BY department.id;
 
--- TO GET MANAGER NAME! let query = "SELECT * FROM employee WHERE manager_id IS NULL";
+-- EMPLOYEE MANAGERS!!!!
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name, employee.manager_id, manager.first_name AS manager_first, manager.last_name AS manager_last FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+LEFT JOIN employee manager ON manager.id = employee.manager_id;
+
+-- VIEW EMPLOYEES
+SELECT employee.id, employee.first_name, employee.last_name, department.department_name, role.salary, manager.first_name AS manager_first, manager.last_name AS manager_last FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+LEFT JOIN employee manager ON manager.id = employee.manager_id;
+
+-- Employees by Department
+SELECT department.id, department.department_name, employee.first_name, employee.last_name, role.title 
+FROM department
+LEFT JOIN employee ON department.id = employee.role_id
+LEFT JOIN role ON department.id = role.department_id;
+
+-- View All Roles
+SELECT role.title, role.salary, role.department_id, department.department_name FROM role
+LEFT JOIN department ON role.department_id = department.id;
